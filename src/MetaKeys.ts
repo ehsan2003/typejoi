2de83d95088f6b7632bc54e78db63930c5be7bc3
ehsan2003 <ehsan2003.2003.382@gmail.com>
2021-03-29 23:30:10 +0430

    codeFormating

--- src/MetaKeys.ts
+++ src/MetaKeys.ts
@@ -10,11 +10,12 @@ export type SchemaModifier = (schema: Joi.ObjectSchema) => Joi.ObjectSchema;
 export type ModifierOrSchema = SchemaModifier | Joi.Schema;
 type MetaTypes = {
     isTypeJoi: boolean;
-    joiKeys: { key: string; schema: Joi.Schema; }[];
+    joiKeys: { key: string; schema: Joi.Schema }[];
     JoiModifier: ModifierOrSchema;
 };
 export function getMeta<K extends keyof MetaTypes, T extends MetaTypes[K]>(
     key: K,
-    target: any): T {
+    target: any
+): T {
     return Reflect.getMetadata(MetaKeys[key], target);
 }
